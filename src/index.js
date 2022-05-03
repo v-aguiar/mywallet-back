737a716718b854611b3f720067dfb74b7e1d4d2f
v-aguiar <v.aguiar.dev@gmail.com>
2022-05-03 15:21:59 -0300

    Initialize node app with Express. Configured to listen on port 5000.

---
+++ src/index.js
@@ -0,0 +1,20 @@
+﻿import express, { json } from "express";
+
+import joi from "joi";
+import cors from "cors";
+import chalk from "chalk";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(json());
+app.use(cors());
+
+app.listen(5000, () => {
+  console.log(
+    chalk.bold.greenBright("\n🚀 Server is running!") +
+      chalk.bold.cyanBright("\n\nListening on port 5000...\n") +
+      chalk.bold.magenta("http://localhost:5000\n")
+  );
+});
